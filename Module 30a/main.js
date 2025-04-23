b6ba83225e3cf78004fdeb8b508463de88b38871
greta <greta.sopa@stu-alg-prizren.com>
2025-04-23 18:39:55 +0200

    hey sisters

---
+++ Module 30a/main.js
@@ -0,0 +1,62 @@
+var wakeuptime;
+var dstime;
+var sleeptime;
+var noon = 12;
+
+function showCurrentTime(){
+    var clock = document.getElementById('clock');
+    var showCurrentTime = new Date();
+
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getseconds();
+
+    var meridian = "AM";
+
+    if(hours >= noon){
+        meridian="PM";
+    }
+
+var clockTime = hours + ":" + minutes + ":" + seconds +" " + meridian;
+clock.innerText = clockTime;
+changeImage();
+
+
+};
+
+var oneSecond = 1000;
+
+setInterval(showCurrentTime, oneSecond);
+
+function changeImage (){
+    var time = new Date().getHours();
+    console.log(time);
+
+    var image= "ds_clock.png";
+    var imageHTML=document.getElementById("timeImage");
+
+if(time== wakeuptime){
+    images = "morning.gif";
+    console.log("morning");
+}
+else if(time==dstime){
+    images = "class.gif";
+}
+else if(time==sleepTime){
+    images = "night.gif";
+}
+imageHTML.src = image;
+console.log(imageHTML.src);
+
+
+}
+function updateClock(){
+    var wakeUpTimeSelector = document.getElementById('wakeUpTimeSelector');
+    wakeuptime=wakeUpTimeSelector.value;
+    var dsTimeSelector = document.getElementById('dsTimeSelector');
+    dstime = dsTimeSelector.value;
+    var sleepTimeSelector=document.getElementById('sleepTimeSelector');
+    sleepTime = sleepTimeSelector.value;
+}
+var saveButton = document.getElementById("saveButton");
+saveButton.addEventListener("click",updateClock);
