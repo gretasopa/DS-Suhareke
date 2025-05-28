1d20c6522887cb0311ffa86a2e3f429b41875ea8
greta <greta.sopa@stu-alg-prizren.com>
2025-05-28 19:19:26 +0200

    `2w

---
+++ Module 36/main1.js
@@ -0,0 +1,50 @@
+$('li').click(function(){
+    alert('clicked');
+});
+
+$('#btn').click(function(){
+    console.log($('#h1').text());
+    $('#h1').text('Testing text');
+    $('#h1').append('Extra text')
+});
+
+$('#btn2').on('click',function(){
+    console.log('Second button is clicked');
+});
+
+$('#myDiv').click(function(){
+    $(this).addClass('clicked');
+})
+.find('span')
+.attr('title','Hover over me');
+
+$('#btn3').click(function(){
+    $(".hidden").hide();
+});
+
+$('#btn4').click(function(){
+    $(".hidden").hide();
+});
+
+$('#square').click(function(){
+
+    $('#square').animate({
+        '#width' : '200px',
+        'height' : '200px'
+    })
+});
+$('.testing').click(function(){
+
+    $('.testing').animate({
+        'left': '+=50px',
+        'opacity': 0.25,
+        'fontSize':'30px'
+    },
+    300,
+    function(){
+        console.log("Animation is completed");
+    }
+);
+});
+
+
