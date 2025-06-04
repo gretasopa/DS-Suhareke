1c773efb10b6ef70abee7d7f9ceb881ec9c99477
greta <greta.sopa@stu-alg-prizren.com>
2025-06-04 18:25:25 +0200

    greeee

---
+++ Module 37/main.js
@@ -0,0 +1,19 @@
+//$('sort').sortable();
+
+//$(function(){
+    //$("accordion").accordion();
+
+//});
+//$('#tabs').tabs();
+
+$(function(){
+    $('#accordion').accordion({
+        collapsible: true
+    });
+});
+
+$(function(){
+    $('#tabs').tabs({
+        collapsible:true
+    });
+});
