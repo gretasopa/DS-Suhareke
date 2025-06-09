4d5712c0137dad6fe6d1e8ee97dc8a3d71bea793
greta <greta.sopa@stu-alg-prizren.com>
2025-06-09 19:21:01 +0200

    sefv

---
+++ Module 30/easy/easy.js
@@ -0,0 +1,8 @@
+document.getElementById('submitBtn').addEventListener('click',()=>{
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    alert("hello"+name+",you are"+age+"years old!");
+
+    
+
+});
