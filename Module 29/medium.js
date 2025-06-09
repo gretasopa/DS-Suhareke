2d63d5adfde603ab1be50bdd745ddbf24a93250e
greta <greta.sopa@stu-alg-prizren.com>
2025-06-09 18:28:43 +0200

    1222222

--- Module 29/medium.js
+++ Module 29/medium.js
@@ -1,4 +1,10 @@
 var programmingLanguage=[1,2,3,4,5];
 
-programmingLanguage.unshift("c++");
+programmingLanguage.push(6);
+console.log(programmingLanguage);
+
+programmingLanguage.shift(1);
+console.log(programmingLanguage);
+
+programmingLanguage.splice(1,1,5);
 console.log(programmingLanguage);
