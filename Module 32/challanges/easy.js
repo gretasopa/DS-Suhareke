5e6dc28f081bbb61c281cfa39c89b84eac75155c
greta <greta.sopa@stu-alg-prizren.com>
2025-06-10 18:16:18 +0200

    123

--- Module 32/challanges/easy.js
+++ Module 32/challanges/easy.js
@@ -1,18 +1,17 @@
-var nameInput=document.getElementById('name');
-var errorMessage=document.getElementById('error');
-var form=document.getElementById('nameForm');
+var nameInput = document.getElementById('name');
+var errorMessage = document.getElementById('error');
+var form = document.getElementById('nameForm');
 
-form.addEventListener('submit',function(event) {
+form.addEventListener('submit', function(event) {
     event.preventDefault();
-    var nameValue=nameInput.value ;
+    var nameValue = nameInput.value;
 
-    if(/[a-zA-Z]/).test(nameValue)){
-        errorMessage.style.display='inline';
-    }else{
-        errorMessage.style.display='none';
-        alert('forma u degua me sukses');
-
+    if (!/^[a-zA-Z]+$/.test(nameValue)) {
+        // Show error if input is not only letters
+        errorMessage.style.display = 'inline';
+    } else {
+        // Hide error and show success
+        errorMessage.style.display = 'none';
+        alert('Forma u dërgua me sukses');
     }
-    });
-
-
+});
